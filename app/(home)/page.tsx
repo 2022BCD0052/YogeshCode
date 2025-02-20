08880fed24825482a6eca689c1a07215458918f8
Yogesh Saini <121495438+2022BCD0052@users.noreply.github.com>
2025-02-20 10:18:13 +0530

    add initial Navbar component and update build manifests

---
+++ app/(home)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <Button>Home</Button>
+
+    </div>
+  )
+}
+
+export default page
